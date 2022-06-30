a1dd2db07dfec490354de63484f3604271d9a5e9
Camie0903 <camerontamboer03@gmail.com>
2022-06-30 15:21:34 +0200

    alomst there

--- admin.js
+++ admin.js
@@ -5,7 +5,7 @@ let listings = JSON.parse(localStorage.getItem("listings"))
         id: "1",
         img:"./assets/hr1.jpg",
         title:"Villa Suite",
-        type:"double",
+        type:"double-story",
         carhead: "Flowered Estate, Brooklyn, USA",
         card_icon: 3,
         card_icon1: 2,
@@ -17,7 +17,7 @@ let listings = JSON.parse(localStorage.getItem("listings"))
         id:"2",
         img:"./assets/hr2.jpg",
         title:"Nice View Studio",
-        type:"double",
+        type:"single-story",
         carhead: "Beach Rd, Las Vegas",
         card_icon: 4,
         card_icon1: 3,
@@ -29,7 +29,7 @@ let listings = JSON.parse(localStorage.getItem("listings"))
         id: "3",
         img:"./assets/hr3.jpg",
         title:"Mykonos-like ",
-        type:"double",
+        type:"double-story",
         carhead: "Elegant Estate, California",
         card_icon: 6,
         card_icon1: 4,
@@ -41,7 +41,7 @@ let listings = JSON.parse(localStorage.getItem("listings"))
         id: "4",
         img:"./assets/hr4.jpg",
         title:"Luxurious Palace",
-        type:"double",
+        type:"double-story",
         carhead: "Main Street, United Kingdom",
         card_icon: 25,
         card_icon1: 30,
@@ -53,7 +53,7 @@ let listings = JSON.parse(localStorage.getItem("listings"))
         id: "5",
         img:"./assets/hr5.jpg",
         title:"Ballroom",
-        type:"double",
+        type:"double-story",
         carhead: "Fancy Diner, Italy",
         card_icon: 94,
         card_icon1: 100,
@@ -65,7 +65,7 @@ let listings = JSON.parse(localStorage.getItem("listings"))
         id: "6",
         img:"./assets/hr6.jpg",
         title:"Bachelor",
-        type:"double",
+        type:"single-story",
         carhead: "Modern Road, Texas",
         card_icon: 4,
         card_icon1: 3,
@@ -87,8 +87,8 @@ function showlistings(listings) {
     <td>${listings.img}<td>
     <td>${listings.title}<td>
     <td>${listings.type}<td>
-    <td>${listings.location}<td>
-    <td>${listings.size}<td>
+    <td>${listings.carhead}<td>
+    <td>${listings.bot_head}<td>
     <td><i class="fa-solid fa-pen-to-square p-3"></i>
     <i onclick="deleteItem(${listings.id})" class="fa-solid fa-trash-can p-3"></i></td>
     <tr>
@@ -102,8 +102,8 @@ function addProperty(){
     img: document.querySelector("#image").value,
     title: document.querySelector("#title").value,
     type:document.querySelector("#type").value,
-    location:document.querySelector("#location").value,
-    size:document.querySelector("#size").value,
+    carhead:document.querySelector("#carhead").value,
+    bot_head   :document.querySelector("#bot_head").value,
     id:listings.length + 1,
   };
   listings.push(newPropety);
@@ -117,4 +117,21 @@ function deleteItem(id){
   listings = listings.filter((listings) => listings.id !== id);
   localStorage.setItem("listings", JSON.stringify(listings));
   showlistings(listings)
+}
+
+function editItems(id) {
+  let img = document.querySelector(`#image-${id}`).value;
+  let title = document.querySelector(`#title-${id}`).value;
+  let type = document.querySelector(`#type-${id}`).value;
+  let carhead = document.querySelector(`#carhead-${id}`).value;
+  let bot_head = document.querySelector(`#bot_head-${id}`).value;
+  listings[id] = {
+    img,
+    title,
+    type,
+    carhead,
+    bot_head
+  };
+  localStorage.setItem("listings", JSON.stringify(listings));
+  readData(listings);
 }
